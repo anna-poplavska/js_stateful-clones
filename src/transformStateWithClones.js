1a29b03605443ac07d8d1f95101563e42f007953
Anna Poplavska <annapoplavska10@gmail.com>
2025-02-05 17:39:17 +0100

    Task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const changedState = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(changedState, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete changedState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in changedState) {
+        delete changedState[key];
+      }
+    }
+    result.push({ ...changedState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
